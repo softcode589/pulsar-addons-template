2b8bd38558aa057242bf5efc8233fcfce0186dd8
softcode <108250871+softcode589@users.noreply.github.com>
2022-09-17 15:39:43 -0600

    Revert "Revert to initial commit"
    This reverts commit 1bbbe8ca4c41e72a754520f52454031e6dfa17cf.

--- spec/benchmark-spec.js
+++ spec/benchmark-spec.js
@@ -1,20 +1,22 @@
 describe("Benchmark", () => {
   // This number doesn't match what timecope gives, but shows the trend
   it("Activation Benchmark", async () => {
-    jasmine.attachToDOM(atom.views.getView(atom.workspace))
-    atom.packages.triggerDeferredActivationHooks()
+    jasmine.attachToDOM(atom.views.getView(atom.workspace));
+    atom.packages.triggerDeferredActivationHooks();
     // Activate activation hook
-    atom.packages.triggerActivationHook("core:loaded-shell-environment")
+    atom.packages.triggerActivationHook("core:loaded-shell-environment");
 
     // For benchmark, activate the deps manually before loading the actual package:
-    const deps = []
-    await Promise.all(deps.map((p) => atom.packages.activatePackage(p)))
+    const deps = [];
+    await Promise.all(deps.map((p) => atom.packages.activatePackage(p)));
 
     // Activate the package
     measure("Activation Time", async function activationBenchmark() {
-      await atom.packages.activatePackage("pulsar-addon-template-js")
-    })
+      await atom.packages.activatePackage("pulsar-addon-template-js");
+    });
 
-    expect(atom.packages.isPackageLoaded("pulsar-addon-template-js")).toBeTruthy()
-  })
-})
+    expect(
+      atom.packages.isPackageLoaded("pulsar-addon-template-js")
+    ).toBeTruthy();
+  });
+});
